a4d0ca20ea64dc735b90d7f6a594c1005a42723e
r0ckwav3 <peterrockwave@gmail.com>
2024-11-15 12:16:19 -0800

    add Course entity file

---
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Course.java
@@ -0,0 +1,32 @@
+package edu.ucsb.cs156.happiercows.entities;
+
+// import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+// import java.time.LocalDateTime;
+// import java.util.List;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String code;
+    private String name;
+    private String term;
+
+    // @OneToMany(mappedBy = "course", cascade = CascadeType.REMOVE)
+    // @JsonIgnore
+    // private List<Student> students;
+}
