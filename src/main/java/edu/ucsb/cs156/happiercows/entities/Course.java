7f9495cbaa0d8b794e80b8b906f6fa68174bdff8
r0ckwav3 <peterrockwave@gmail.com>
2024-11-19 17:23:52 -0800

    remove commented code

--- src/main/java/edu/ucsb/cs156/happiercows/entities/Course.java
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Course.java
@@ -1,14 +1,11 @@
 package edu.ucsb.cs156.happiercows.entities;
 
-// import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-// import java.time.LocalDateTime;
-// import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -23,8 +20,4 @@ public class Course {
     private String code;
     private String name;
     private String term;
-
-    // @OneToMany(mappedBy = "course", cascade = CascadeType.REMOVE)
-    // @JsonIgnore
-    // private List<Student> students;
 }
